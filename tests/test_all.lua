6379f13f235db4b56473bf5ea4bbb6388730ac9a
seikarii <karlosseika@gmail.com>
2026-09-08 06:55:30 +0200

    Update benchmark results and add spell info state test to the test suite

--- tests/test_all.lua
+++ tests/test_all.lua
@@ -7,6 +7,7 @@
 local tests = {
     "tests/smoke_test.lua",
     "tests/classification_test.lua",
+    "tests/spell_info_state_test.lua",
     "tests/spell_resolver_test.lua",
     "tests/utils_test.lua",
     "tests/interrupt_test.lua",
